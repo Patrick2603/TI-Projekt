f7b8460d754d0103a3d660686b36ea8bdd9eb1a3
Patrick Schöttle <patrick.p.schoettle@gmail.com>
2023-05-05 14:00:10 +0200

    setup Platformio + MQTT client

---
+++ TI_Projekt/include/main.h
@@ -0,0 +1,4 @@
+esp_mqtt_client_config_t mqtt_cfg = {
+    .uri;
+}
+;
---
+++ TI_Projekt/include/myData.h
@@ -0,0 +1,25 @@
+/**
+ * @file myData.h
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-05-05
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifndef WIFI_CREDENTIALS_H
+#define WIFI_CREDENTIALS_H
+
+#define SSID "yourSSID"
+#define PASS "yourPassword"
+
+#endif
+
+#ifndef MQTT_CREDENTIALS_H
+#define MQTT_CREDENTIALS_H
+
+#define BROKER "mqtt://test.mosquitto.org:1883"
+
+#endif
